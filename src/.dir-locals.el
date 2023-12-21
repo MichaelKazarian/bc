91331d75b44364d81586513a75bfe3e1c2e64cd4
MichaelKazarian <28EMACSlisp>
2023-12-21 14:44:28 +0200

    .dir-locals.el update

--- src/.dir-locals.el
+++ src/.dir-locals.el
@@ -7,13 +7,19 @@
 ;; Requirements: 
 ;; Status: not intended to be distributed yet
 
-((nil . ((company-clang-arguments . (
-                                     "-I~/.platformio/packages/framework-arduino-avr"
-                                     "-I~/bc/.pio/libdeps/pro16MHzatmega328"
-                                     "-I~/bc/src"))
-         (eval . (progn
+
+((nil . ((eval . (progn
+                   (setq-local company-clang-arguments (clang-dirs))
+                   (setq-local tags-candidate
+                               '("~/.platformio/packages/framework-arduino-avr/cores/arduino"
+                                 "~/bc/.pio/libdeps/pro16MHzatmega328/LiquidCrystal/src"
+                                 "~/bc/.pio/libdeps/pro16MHzatmega328/DallasTemperature"
+                                 "~/bc/src"))
                    (add-to-list 'company-backends '(company-etags company-clang))
-                   (add-hook 'after-save-hook 'etags-tag-create)))
+                   (add-hook 'after-save-hook
+                             (lambda ()
+                               (etags-tag-create (find-dirs-str))))
+                   ))
          (eval . (git-gutter-mode))
          (eval . (yas-minor-mode-on))
          (eval . (message ".dir-locals.el was loaded"))
